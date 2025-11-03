051cf7b34842ea227887ec01a6a3dd10207b65fc
wraith0905 <maksymberezhnoy95@gmail.com>
2025-11-04 06:33:03 +0900

    feat: edit product, log for specific users

---
+++ src/app/pages/dashboards/components/bannerwidget.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppShipmentView } from '@/layout/components/app.shipmentView';
+import { CardModule } from 'primeng/card';
+import { AuthService } from '@/auth/auth.service';
+
+@Component({
+    selector: 'banner-widget',
+    standalone: true,
+    imports: [CommonModule, CardModule],
+    templateUrl: './bannerwidget.component.html',
+    host: {
+        class: 'flex w-full flex-wrap gap-7'
+    },
+    providers: [AppShipmentView],
+})
+export class BannerWidget implements OnInit {
+
+    isTestUser = signal(false);
+
+    constructor(private authService: AuthService) { }
+
+    ngOnInit() {
+        const email = this.authService.currentUserValue?.email;
+        console.log("email", email);
+
+        if (email == 'testuser@gmail.com' || email == 'kevin.sambirsky@gmail.com') {
+            this.isTestUser.set(true);
+            console.log("isTestUser", this.isTestUser);
+            
+        } else {
+            this.isTestUser.set(false);
+        }
+    }
+
+}
--- src/app/pages/product/product.route.ts
+++ src/app/pages/product/product.route.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductCreateComponent } from './productCreate/product-create.component';
 import { ProductList } from './productlist/productlist.component';
 import { ProductOverview } from "./productoverview/productoverview";
+import { ProductUpdateComponent } from './productUpdate/product-update.component';
 
 export default [
     { path: '', data: { breadcrumb: 'Product List' }, component: ProductList },
     { path: 'create', data: { breadcrumb: 'Create Product' }, component: ProductCreateComponent },
-    { path: 'overview/:id', data: { breadcrumb: 'Product Overview' }, component: ProductOverview }
+    { path: 'overview/:id', data: { breadcrumb: 'Product Overview' }, component: ProductOverview },
+    { path: 'update/:id', data: { breadcrumb: 'Update Product' }, component: ProductUpdateComponent }
 ] as Routes;
