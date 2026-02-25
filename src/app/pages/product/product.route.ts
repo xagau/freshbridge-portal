0c5e3176fd221dbd9deb1d2df4086ffced6aea02
maksym-agetsllc <maksym@agts.llc>
2026-02-25 13:46:16 -0600

    feat: multi selector for product list

--- src/app/pages/product/product.route.ts
+++ src/app/pages/product/product.route.ts
@@ -5,7 +5,7 @@ import { ProductOverview } from "./productoverview/productoverview";
 import { ProductUpdateComponent } from './productUpdate/product-update.component';
 
 export default [
-    { path: '', data: { breadcrumb: 'Product List' }, component: ProductList },
+    { path: '', data: { breadcrumb: 'Merchant & Products' }, component: ProductList },
     { path: 'create', data: { breadcrumb: 'Create Product' }, component: ProductCreateComponent },
     { path: 'overview/:id', data: { breadcrumb: 'Product Overview' }, component: ProductOverview },
     { path: 'update/:id', data: { breadcrumb: 'Update Product' }, component: ProductUpdateComponent }
